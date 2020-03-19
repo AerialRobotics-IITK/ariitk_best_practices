57e3139b4a88f0dfeddf9c0297494066173b1cff
Ashwin Shenai <ashwins@iitk.ac.in>
2020-03-19 10:25:07 +0530

    fixed indentation

--- ros_package_template/include/ros_package_template/template_ros.hpp
+++ ros_package_template/include/ros_package_template/template_ros.hpp
@@ -4,25 +4,27 @@
 #include <lib_package_template/template.hpp>
 
 namespace ariitk::ros_package_template {
-    class TemplateROS {
-        public:
-            TemplateROS()
-                : cloud_(new pcl::PointCloud<pcl::PointXYZ>) {};
-            void init(ros::NodeHandle& nh);
-            void run();
 
-        private:
-            void cloudCallback(const sensor_msgs::PointCloud2& msg);
+class TemplateROS {
+    public:
+        TemplateROS()
+            : cloud_(new pcl::PointCloud<pcl::PointXYZ>) {};
+        void init(ros::NodeHandle& nh);
+        void run();
 
-            ros::Publisher pc_fit_pub_;
+    private:
+        void cloudCallback(const sensor_msgs::PointCloud2& msg);
 
-            ros::Subscriber pc_sub_;
+        ros::Publisher pc_fit_pub_;
 
-            pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_;
+        ros::Subscriber pc_sub_;
 
-            ariitk::lib_package_template::ChildTemplatePlane plane_;
-            ariitk::lib_package_template::ChildTemplateSphere sphere_;
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_;
 
-            bool do_plane_;
-    };
-}
+        ariitk::lib_package_template::ChildTemplatePlane plane_;
+        ariitk::lib_package_template::ChildTemplateSphere sphere_;
+
+        bool do_plane_;
+};
+
+} // namespace ariitk::ros_package_template
--- ros_package_template/include/ros_package_template/template_ros_pub.hpp
+++ ros_package_template/include/ros_package_template/template_ros_pub.hpp
@@ -4,24 +4,26 @@
 #include <lib_package_template/template.hpp>
 
 namespace ariitk::ros_package_template {
-    class TemplateROSPub {
-        public:
-            TemplateROSPub()
-                : cloud_(new pcl::PointCloud<pcl::PointXYZ>) {};
-            void init(ros::NodeHandle& nh);
-            void run();
 
-        private:
-            ros::Publisher pc_pub_;
+class TemplateROSPub {
+    public:
+        TemplateROSPub()
+            : cloud_(new pcl::PointCloud<pcl::PointXYZ>) {};
+        void init(ros::NodeHandle& nh);
+        void run();
 
-            pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_;
+    private:
+        ros::Publisher pc_pub_;
 
-            ariitk::lib_package_template::ChildTemplatePlane plane_;
-            ariitk::lib_package_template::ChildTemplateSphere sphere_;
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_;
 
-            bool do_plane_;
+        ariitk::lib_package_template::ChildTemplatePlane plane_;
+        ariitk::lib_package_template::ChildTemplateSphere sphere_;
 
-            int width_;
-            int height_;
-    };
-}
+        bool do_plane_;
+
+        int width_;
+        int height_;
+};
+
+} // namespace ariitk::ros_package_template
