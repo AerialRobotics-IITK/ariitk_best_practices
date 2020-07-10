007c6d495ad332c69011b1c476beac15b6a5c10c
Kshitij1K <kabeer@iitk.ac.in>
2020-07-10 21:06:47 +0530

    Make benchmarking more efficient
    Instead of manually passing the values to the ORB_create function, now
    it is automatically generates the sets of parameters and prints all
    results in LaTeX table format.

--- BENCHMARKS/ORB_feature_detector_algorithm/ORB_feature.py
+++ BENCHMARKS/ORB_feature_detector_algorithm/ORB_feature.py
@@ -9,6 +9,7 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 from line_profiler import LineProfiler
+from itertools import product
 
 # This function takes the create orb object and an image frame, and outputs the feature keypoints in the frame 
 def getKeyPoints(orb, frame):
@@ -16,7 +17,7 @@ def getKeyPoints(orb, frame):
     kp, des = orb.compute(frame, kp)
     return kp
 
-def main():
+def main(program_dir, scale_factor, wta_k, score_type):
     # num_features_list stores the number of features detected in each frame.
     # time stores the time taken in miliseconds to get features in each frame, by the function getKeyPoints
     # num_frames stores the number of frames  
@@ -24,13 +25,12 @@ def main():
     time = []
     num_frames = 0
 
-    # Created the ORB feature matcher object, with maximum number of detected features as the argument. You can either limit the 
-    # number of features to save computation, or put a very high number, in which case it will detect as many features as it can.
-    orb = cv2.ORB_create(nfeatures=10000)
+    # Created the ORB feature matcher object, with the requisite arguments (some default some taken from command line)
+    orb = cv2.ORB_create(nfeatures=10000, edgeThreshold=50, patchSize=50, fastThreshold=20, scaleFactor=scale_factor, WTA_K=wta_k ,scoreType=score_type)
 
     # Place your data set in the BENCHMARKS/ORB_feature_detector_algorithm/datasets directory. Make sure it is encoded in FFMPEG
     # format for OpenCV to use it.
-    cap = cv2.VideoCapture(os.path.dirname(os.sys.argv[0]) + '/datasets/Raw.mp4')
+    cap = cv2.VideoCapture(program_dir + '/datasets/Raw.mp4')
     if cap.isOpened() == False:
         print("Error opening video file.")
         return
@@ -62,24 +62,19 @@ def main():
         num_features_list.append(len(kp))
 
         frame = cv2.drawKeypoints(frame, kp, None, color=(0, 255, 0), flags=0)
-        cv2.imshow("FEATURES", frame)
-
-        if cv2.waitKey(25) & 0xff == ord('q'):
-            break
 
     cap.release()
-    cv2.destroyAllWindows()
 
     frame_list = range(1, num_frames+1)
 
-    # This part prepares plots of the requisite data, and saves it in an SVG file
+    # This part prepares plots of the requisite data, and saves it in an SVG file, which is unique to the parameters passed.
     plt.figure(figsize=(25, 10))
     plt.plot(frame_list, num_features_list)
     plt.xlabel('Frame Number')
     plt.ylabel('Detected Features')
     plt.title('Plot of Detected Features in Each Frame')
     plt.tight_layout()
-    plt.savefig(os.path.dirname(os.sys.argv[0]) + '/plots/features_number_plot.svg')
+    plt.savefig(program_dir + '/plots/features_number_plot_' + str(scale_factor) + '_' + str(wta_k) + '_' + ('HARRIS' if i[2] == cv2.ORB_HARRIS_SCORE else 'FAST') + '.svg')
 
     plt.clf()
     
@@ -89,7 +84,37 @@ def main():
     plt.ylabel('Time Taken for Identifying Features(ms)')
     plt.title('Plot of Time Taken for Identifying Features in Each Frame')
     plt.tight_layout()
-    plt.savefig(os.path.dirname(os.sys.argv[0]) + '/plots/time_plot.svg')
+    plt.savefig(program_dir + '/plots/time_plot_' + str(scale_factor) + '_' + str(wta_k) + '_' + ('HARRIS' if i[2] == cv2.ORB_HARRIS_SCORE else 'FAST') + '.svg')
+    
+    plt.close('all')
+
+    # Returns the average time taken/frame and the average number of features detected/frame
+    return sum(time)/len(time), sum(num_features_list)/len(num_features_list)
 
 if __name__ == "__main__":    
-    main()
+    # This is list of the range of variables that you want to benchmark.
+    # First list is the list of scaleFactor values to be tested.
+    # Second list is the list of WTA_K values to be tested.
+    # Third list is the list of scoreType values to be tested.
+    list_args = [[1.2, 1.5, 2, 3], [2, 3, 4], [cv2.ORB_HARRIS_SCORE, cv2.ORB_FAST_SCORE]]
+    
+    # This produces all the possible combinations of these parameters, for benchmarking
+    comb_args = list(product(*list_args))
+
+    # os.path.dirname returns None if the current directory is .
+    # To circumvent that this entire code block was written :(
+    path = os.path.dirname(os.sys.argv[0])
+    if path == '':
+        path = '.'
+
+    for i in comb_args:
+        computation, features = main(path, i[0], i[1], i[2])
+
+        # This really long print statement is for printing the results in a format a LaTeX table can understand, so that you can automatically port it to a LaTeX table
+        # Automation 100 :)
+        print(i[0], '&',\
+              i[1], '&',\
+              'HARRIS\\_SCORE' if i[2] == cv2.ORB_HARRIS_SCORE else 'FAST\\_SCORE', '&',\
+              '%.2f'%(computation), '&',\
+              '%.2f'%(features), '\\\\', \
+              sep=' ')
